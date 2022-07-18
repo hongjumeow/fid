994e543867c74c9e4d3dd51b218b3169e919a896
Hongju Meow <8851218@hyundai.com>
2022-07-18 11:32:37 +0900

    read files in broader range

--- fid.py
+++ fid.py
@@ -12,8 +12,22 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--dataset1', type=str)
 parser.add_argument('--dataset2', type=str)
 
-def preprocess_before_eval(base_dir, img):
-    path = os.path.join(base_dir, img)
+def is_image_file(name):
+    if name.endswith('.jpg' or '.png'):
+        return True
+    else:
+        return False
+
+def walk_dirs_and_append(dir):
+    images = []
+    for root, dirs, fnames in sorted(os.walk(dir, followlinks=True)):
+        for fname in fnames:
+            if is_image_file(fname):
+                path = os.path.join(root, fname)
+                images.append(path)
+    return images
+
+def preprocess_before_eval(path):
     im = Image.open(path)
     preprocess= transforms.Compose([
         transforms.Resize(299),
@@ -24,7 +38,6 @@ def preprocess_before_eval(base_dir, img):
     input1 = preprocess(im)
     return input1.unsqueeze(0)
 
-
 def compute_fid(dataset1, dataset2):
     model = torch.hub.load('pytorch/vision:v0.10.0', 'inception_v3', pretrained=True)
     model.eval()
@@ -33,17 +46,20 @@ def compute_fid(dataset1, dataset2):
     file1 = os.listdir(dataset1)
     file2 = os.listdir(dataset2)
 
-    num_images = len(file2)
-    if len(file1) < len(file2):
-        num_images = len(file1)
+    images1 = walk_dirs_and_append(dataset1)
+    images2 = walk_dirs_and_append(dataset2)
+
+    num_images = len(images1)
+    if len(images1) > len(images2):
+        num_images = len(images2)
     
     activations1 = np.zeros((num_images, 1000), dtype=np.float32)
     activations2 = np.zeros((num_images, 1000), dtype=np.float32)
 
     print("Calculating Activations for each images...")
     for i in tqdm(range(num_images)):
-        input1 = preprocess_before_eval(dataset1, file1[i])
-        input2 = preprocess_before_eval(dataset2, file2[i])
+        input1 = preprocess_before_eval(images1[i])
+        input2 = preprocess_before_eval(images2[i])
 
         with torch.no_grad():
             act1 = model(input1)
